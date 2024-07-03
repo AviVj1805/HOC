e782b8c8f9bb2b10b2c92e2fdee9284284867b66
AviVj1805 <147787174+AviVj1805@users.noreply.github.com>
2024-07-03 11:45:24 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+// App.js
+
+import React from 'react';
+import UserList from './UserList';
+import PostList from './PostList';
+
+const App = () => (
+  <div>
+    <h1>App</h1>
+    <UserList />
+    <PostList />
+  </div>
+);
+
+export default App;
